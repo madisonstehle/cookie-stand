3de7b3c2aab1f2b280db392f24990245eb986fb9
Madison Stehle <mnstehle3@gmail.com>
2019-11-09 15:50:31 -0800

    added loop and randomNumberGenerator to seattleShop object in js

--- js/app.js
+++ js/app.js
@@ -3,13 +3,30 @@
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var seattleElement = document.getElementById('seattle');
-var liEl = document.createElement('li');
-liEl.textContent = 'proof of life';
-seattleElement.appendChild(liEl);
 
 var seattleShop = {
+    minCust: 23,
+    maxCust: 65,
+    avgCookieSale: 6.3,
+    customersEachHour: [],
+    cookiesEachHour: [],
+    totalCookiesForDay: 0,
+    
+    cookiesEachHour: function() {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+    },
 
+    render: function() {
+        for (var i = 0; i < hours.length; i++) {
+            var liEl = document.createElement('li');
+            liEl.textContent = `${hours[i]} cookies: ${this.cookiesEachHour()}`;
+            seattleElement.appendChild(liEl);
+            // console.log(i);
+        }
+        return liEl;
+    }
 }
+seattleShop.render();
 
 var tokyoShop = {
     
