663861841214a14b8d0306ea8a09a7f1e818a79f
Madison Stehle <mnstehle3@gmail.com>
2019-11-09 14:50:54 -0800

    added nav links between index and sales, barebones js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var seattleElement = document.getElementById('seattle');
+var liEl = document.createElement('li');
+liEl.textContent = 'proof of life';
+seattleElement.appendChild(liEl);
+
+var seattleShop = {
+
+}
+
+var tokyoShop = {
+    
+}
+
+var dubaiShop = {
+    
+}
+
+var parisShop = {
+    
+}
+
+var limaShop = {
+    
+}
