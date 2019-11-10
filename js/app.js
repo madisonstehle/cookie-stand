97a23ae8cf52fd2a4796aed8b5d1a81ba8d35104
Madison Stehle <mnstehle3@gmail.com>
2019-11-10 11:15:55 -0800

    added js data for Tokyo, works

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+// SEATTLE STORE
 var seattleElement = document.getElementById('seattle');
 
 var seattleShop = {
@@ -39,9 +40,42 @@ var seattleShop = {
 
 seattleShop.render();
 
+// TOKYO STORE
+var tokyoElement = document.getElementById('tokyo');
+
 var tokyoShop = {
+    minCust: 3,
+    maxCust: 24,
+    avgCookieSale: 1.2,
+    customersEachHour: [],
+    cookiesEachHour: [],
+    totalCookiesForDay: 0,
     
+        randomCustomerNumber: function() {
+        return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
+    },
+
+    render: function() {
+        for (var i = 0; i < hours.length; i++) {
+            this.customersEachHour.push(this.randomCustomerNumber());
+
+            var hourlyCookies = this.customersEachHour[i]*this.avgCookieSale;
+            hourlyCookies = Math.round(hourlyCookies);
+            this.cookiesEachHour.push(hourlyCookies);
+
+            this.totalCookiesForDay += this.cookiesEachHour[i];
+
+            var liEl = document.createElement('li');
+
+            liEl.textContent = `${hours[i]} cookies: ${this.cookiesEachHour[i]}`;
+
+            tokyoElement.appendChild(liEl);
+        }
+        liEl.textContent = `Total for the Day: ${this.totalCookiesForDay}`;
+        return liEl;
+    }
 }
+tokyoShop.render();
 
 var dubaiShop = {
     
